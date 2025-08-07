3fdc953f3d6a9a36fcaa4f02238dfcc03455c4e8
ansh-info <anshkumar.info@gmail.com>
2025-08-07 22:37:48 +0200

    fix: udpate module imports

--- aiagents4pharma/talk2scholars/tools/paper_download/__init__.py
+++ aiagents4pharma/talk2scholars/tools/paper_download/__init__.py
@@ -5,16 +5,8 @@ biorxiv and medrxiv.
 """
 
 # Import modules
-from . import arxiv_downloader
-from . import base_paper_downloader
-from . import medrxiv_downloader
 from . import paper_downloader
-from . import pubmed_downloader
 
 __all__ = [
-    "arxiv_downloader",
-    "base_paper_downloader",
-    "medrxiv_downloader",
     "paper_downloader",
-    "pubmed_downloader",
 ]
